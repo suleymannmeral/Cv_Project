81f25d11c3d334e7136b87d96ef40093f727d4f8
suleymannmeral <mrlslymn02@gmail.com>
2024-10-21 22:04:17 +0300

    BusinessLayer Abstract Operations

--- DataAccessLayer/Repository/GenericRepository.cs
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,52 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repository
 {
-    internal class GenericRepository
+    public class GenericRepository<T> : IGenericDAL<T> where T : class
     {
+        public void Delete(T t)
+        {
+            using var c = new Context();
+            c.Remove(t);
+            c.SaveChanges();
+
+        }
+
+        public T GetById(int id)
+        {
+           using var c = new Context();
+           var entity=c.Set<T>().Find(id);
+
+            if(entity == null)
+            {
+                throw new Exception($"Entity with id {id} not found.");
+            }
+            else
+            {
+                return entity;
+            }
+
+        }
+
+        public List<T> GetList()
+        {
+            using var c = new Context();
+            return c.Set<T>().ToList();
+
+
+        }
+
+        public void Insert(T t)
+        {
+            using var c = new Context();
+             c.Add(t);
+            c.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            using var c = new Context();
+            c.Update(t);
+            c.SaveChanges();
+        }
     }
 }
