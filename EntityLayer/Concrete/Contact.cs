d1cfeff7c633b54f05a16eca83657a4bbce2655c
suleymannmeral <mrlslymn02@gmail.com>
2024-10-21 16:01:09 +0300

    Entity Framework DAL created

--- EntityLayer/Concrete/Contact.cs
+++ EntityLayer/Concrete/Contact.cs
@@ -2,21 +2,19 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    public class About
+    public class Contact
     {
         [Key]
-        public int AboutID { get; set; }
-        public string? Title { get; set; } 
-        public string? Description { get; set; } 
-        public int Age { get; set; }
+        public int   ContactID { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
         public string? Mail { get; set; }
         public string? Phone { get; set; }
-        public string? Adress { get; set; }
-        public string? ImageUrl { get; set; }
     }
 }
--- EntityLayer/Concrete/Education.cs
+++ EntityLayer/Concrete/Education.cs
@@ -7,15 +7,14 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    public class Feature
+   public class Education
     {
         [Key]
-        public int FeatureID { get; set; }
-        public string? Header { get; set; }
-        public string? Name { get; set; }
+        public int EducationID { get; set; }
         public string? Title { get; set; }
-
-
+        public string? Name { get; set; }
+        public string? Date { get; set; }
+        public string? ImageUrl { get; set; }
 
     }
 }
