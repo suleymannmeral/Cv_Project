b37b0ee4623dcdb44f53b21d3cce06ef3a84feab
suleymannmeral <mrlslymn02@gmail.com>
2024-10-22 20:00:30 +0300

    Template Edited

--- BusinessLayer/Abstract/IFeatureService.cs
+++ BusinessLayer/Abstract/IFeatureService.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Abstract
 {
-    internal interface IFeatureService:IGenericService<Feature>
+    public interface IFeatureService:IGenericService<Feature>
     {
     }
 }
---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDAL _aboutDal;
+
+        public AboutManager(IAboutDAL aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+
+        public void TAdd(About t)
+        {
+            _aboutDal.Insert(t);
+        }
+
+        public void TDelete(About t)
+        {
+            _aboutDal.Delete(t);
+        }
+
+        public About TGetBYID(int id)
+        {
+           return _aboutDal.GetById(id);
+        }
+
+        public List<About> TGetList()
+        {
+          return   _aboutDal.GetList();
+
+        }
+
+        public void TUpdate(About t)
+        {
+            _aboutDal.Update(t);
+        }
+    }
+}
