03e0c453d55f309f0f8566c736357b7c5c5e539a
suleymannmeral <mrlslymn02@gmail.com>
2024-11-25 17:00:22 +0300

    About Area Completed (Delete,Edit)

---
+++ Core_Project/Controllers/AboutController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EFAboutDAL()); 
+        public IActionResult Index()
+        {
+            var values = aboutManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult EditAbout(int id)
+        {
+            var values=aboutManager.TGetBYID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditAbout(About about)
+        {
+            aboutManager.TUpdate(about);
+            return RedirectToAction("Index");
+        }
+    }
+}
