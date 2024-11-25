e4d607cca79ae0f432555a7019cce0e697e845b9
suleymannmeral <mrlslymn02@gmail.com>
2024-11-25 20:22:51 +0300

    Service Area Completed

---
+++ Core_Project/Controllers/ServicesController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class ServicesController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EFServiceDAL());
+        public IActionResult Index()
+        {
+            var values = serviceManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddService()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddService(Service service)
+        {
+            serviceManager.TAdd(service);
+            return RedirectToAction("Index");  
+        }
+        public IActionResult DeleteService(int id)
+        {
+            var values = serviceManager.TGetBYID(id);
+            serviceManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditService(int id)
+        {
+            var values = serviceManager.TGetBYID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditService(Service service)
+        {
+            serviceManager.TUpdate(service);
+            return RedirectToAction("Index");
+        }
+    }
+}
