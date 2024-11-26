0d2d6a4c5fcfc43c04073830a9d91b788ad1c1ef
suleymannmeral <mrlslymn02@gmail.com>
2024-11-26 18:02:15 +0300

    ADmin sidebar edited

--- Core_Project/Controllers/FeatureController.cs
+++ Core_Project/Controllers/FeatureController.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Project.Controllers
 {
     public class FeatureController : Controller
     {
+        FeatureManager featureManager = new FeatureManager(new EFFeatureDAL());
         public IActionResult Index()
         {
-            return View();
+            var values = featureManager.TGetList();
+            return View(values);
         }
     }
 }
