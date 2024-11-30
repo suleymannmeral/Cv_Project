c3512c2bd901de0339d8a91ae43127df140f580d
suleymannmeral <mrlslymn02@gmail.com>
2024-11-30 12:14:28 +0300

    Validation Classes created

---
+++ BusinessLayer/ValidationRules/FeatureValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class FeatureValidator:AbstractValidator<Feature>
+    {
+        public FeatureValidator()
+        {
+            RuleFor(x => x.Header).NotEmpty().WithMessage("Header Can Not Be Empty");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name Can Not Be Empty");
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Title Can Not Be Empty");
+
+
+        }
+    }
+}
---
+++ BusinessLayer/ValidationRules/SkillValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class SkillValidator:AbstractValidator<Skill>
+    {
+        public SkillValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Title Can Not Be Empty");
+            RuleFor(x => x.Value).NotEmpty().WithMessage("Value Can Not Be Empty");
+            RuleFor(x => x.imageUrl).NotEmpty().WithMessage("ImageUrl Can Not Be Empty");
+
+        }
+    }
+}
