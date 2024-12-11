d43a9eecf0361f2ad0c830ed16822dcda841bcad
suleymannmeral <mrlslymn02@gmail.com>
2024-12-11 15:41:53 +0300

    Dashboard Project List Completed

--- Core_Project/ViewComponents/Dashboard/StatisticsDashboard2.cs
+++ Core_Project/ViewComponents/Dashboard/StatisticsDashboard2.cs
@@ -9,6 +9,8 @@ namespace Core_Project.ViewComponents.Dashboard
         public IViewComponentResult Invoke()
         {
             ViewBag.ss1 = c.Portfolios.Count(); //Project Count
+            ViewBag.ss2 = c.Services.Count(); //Services Count
+            ViewBag.ss3 = c.Messages.Count(); //Services Count
             return View();
         }
     }
