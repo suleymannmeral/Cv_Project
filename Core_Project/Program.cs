550e46bbe5d982fcca3e1de41558688b3ec77225
suleymannmeral <mrlslymn02@gmail.com>
2024-12-14 21:49:56 +0300

    Writer areas created

--- Core_Project/Program.cs
+++ Core_Project/Program.cs
@@ -19,9 +19,17 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Default}/{action=Index}/{id?}"
+    );
+});
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
